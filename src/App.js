4ee1b77e72b3395f842431a9183ec5c324e80fee
GabrielaParedes27 <78928248+GabrielaParedes27@users.noreply.github.com>
2021-05-10 19:08:33 -0500

    added some css

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="dictionary-logo" alt="logo" />
         <h3>What word are you looking for?</h3>
       </header>
-    <SearchEngine />
+    <SearchEngine defaultKeyword="Serendipity" />
     </div>
   );
 }
--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -3,31 +3,46 @@ import Results from "./Results";
 import axios from "axios";
 import "./SearchEngine.css";
 
-export default function SearchEngine(){
-    let [keyword, setKeyword] = useState("");
+export default function SearchEngine(props){
+    let [keyword, setKeyword] = useState(props.defaultKeyword);
     let [results, setResults] = useState(null);
+    let [loaded, setLoaded] = useState(false);
 
     function handleResponse(response){
         setResults(response.data[0]);
     }
 
-    function search(event) {
-        event.preventDefault();
-        
+    function search() {
         let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
         axios.get(apiUrl).then(handleResponse);
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        search();
     }
 
     function handleKeywordChange(event) {
         setKeyword(event.target.value);
     }
 
+    function load() {
+        setLoaded(true);
+        search();
+    }
+
+    if (loaded) {
+
     return (
         <div className="SearchEngine">
-            <form onSubmit={search}>
-                <input className="searching-bar" type="search" onChange={handleKeywordChange} />
+            <form onSubmit={handleSubmit}>
+                <input type="search" onChange={handleKeywordChange} defaultValue="Search for a word (try: wine, plant, yoga)" />
                 <Results results={results} />
             </form>
         </div>
     );
+    } else {
+        load();
+        return "loading";
+    }
 }
